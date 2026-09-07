05662f053f3c0093d7a8c7220677df034242d481
milossdlc <128077325+milossdlc@users.noreply.github.com>
2026-09-07 20:59:45 +0200

    Load navigation polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/global.css";
 import "./styles/actionPlan.css";
 import "./styles/todayPolish.css";
+import "./styles/navigationPolish.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
